3d0020c523795612ab16df783f6c83ca3b56e3da
zellman01 <zellman01@gmail.com>
2018-03-29 09:16:56 -0400

    Update chat.js

--- chat.js
+++ chat.js
@@ -295,6 +295,7 @@ class CommandContext {
 				// @ts-ignore
 				let emoticons = OCPU.parseEmoticons(message);
 				if (emoticons && !this.room.disableEmoticons) {
+					// @ts-ignore
 					if (!OCPU.ignoreEmotes[this.user.userid]) this.user.sendTo(this.room, (this.room.type === 'chat' ? '|c:|' + (~~(Date.now() / 1000)) + '|' : '|c|') + this.user.getIdentity(this.room.id) + '|/html ' + emoticons);
 					// @ts-ignore
 					if (OCPU.ignoreEmotes[this.user.userid]) this.user.sendTo(this.room, (this.room.type === 'chat' ? '|c:|' + (~~(Date.now() / 1000)) + '|' : '|c|') + this.user.getIdentity(this.room.id) + '|' + message);
