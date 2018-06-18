3fc6af5462e658f73624ca8b6294ffde6cbae939
Salem Kallien <31890883+SalemKitkat@users.noreply.github.com>
2018-06-18 16:07:25 +0100

    since bash is dumb

--- ocpu-plugins/custom-commands.js
+++ ocpu-plugins/custom-commands.js
@@ -5,4 +5,10 @@ exports.commands = {
 		// @ts-ignore
 		this.parse("¯\\_(ツ)_/¯");
 	},
+	upd: function (target, room, user, connection) {
+		if (!user.hasConsoleAccess(connection)) {
+			return this.errorReply("/upd - Access denied.");
+		}
+		require('child_process').exec('git pull', (error, stdout, stderr) => {connection.sendTo(room, ("" + stdout + stderr));});
+	},
 };
