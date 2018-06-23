e09ca9409dd8058344768928e8ac0692293b0099
zellman01 <zellman01@gmail.com>
2018-06-23 15:58:10 -0400

    Update custom-commands.js

--- ocpu-plugins/custom-commands.js
+++ ocpu-plugins/custom-commands.js
@@ -17,4 +17,14 @@ exports.commands = {
 		}
 		require('child_process').exec('git stash', (error, stdout, stderr) => {connection.sendTo(room, ("" + stdout + stderr));});
 	},
+	shinymon: function (target, room, user) {
+		if (!target) return this.errorReply("This command requires a target.");
+		if (!this.runBroadcast()) return;
+		this.sendReplyBox(`<img src="https://img.pokemondb.net/sprites/x-y/shiny/${target}.png" width=150,height=200>`);
+	},
+	alolashiny: function (target, room, user) {
+		if (!target) return this.errorReply("This command requires a target.");
+		if (!this.runBroadcast()) return;
+		this.sendReplyBox(`<img src="https://img.pokemondb.net/sprites/sun-moon/dex/shiny/${target}.png" width=150,height=200>`);
+	},
 };
